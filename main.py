799b0f6da378c0229f01e6bc067d142993ec30ba
eduardo-salazar <esalazar922@gmail.com>
2017-06-29 00:18:26 +0800

    Resutls

--- main.py
+++ main.py
@@ -5,45 +5,61 @@ import time
 # 2563976967
 tweets_helper = tweetsHelper.GetTweets()
 
-users = []
-users.append(2563976967)
+#Pro
+target_users = tweets_helper.get_target_users()
+
+#Test
+# target_users = []
+# target_users.append(2563976967)
+
+print("Total users are")
+print(str(len(target_users)))
 
 timestr = time.strftime("%Y%m%d-%H%M%S")
 fileName = "output/pi-"+timestr+".csv"
 
 #Create Begining of file
 f = open(fileName, 'w')
-f.write("user,openness,conscientiousness,extraversion,agreeableness,neuroticism\n")  # python will convert \n to os.linesep
+f.write("user,tweets,openness,conscientiousness,extraversion,agreeableness,neuroticism\n")  # python will convert \n to os.linesep
 f.close()
 
 
-for index, user in enumerate(users):
-    user_tweets = tweets_helper.get_tweets_from(user)
+for index, user in enumerate(target_users):
+    if(index + 1 > 1378):
+        try:
+            print("("+str(index+1)+" of "+str(len(target_users))+")Analysis of user "+str(user))
+            user_tweets = tweets_helper.get_tweets_from(user)
 
-    print("("+str(index+1)+" of "+str(len(users))+")Getting Personality Insight from user "+str(user))
-    personalityInsight = pi.GetPersonalityInsight(user_tweets)
-    user_pi = personalityInsight.call()
+            print("Trying to get Personality Insight From IBM Watson")
+            personalityInsight = pi.GetPersonalityInsight(user_tweets)
+            user_pi = personalityInsight.call()
 
-    traits = user_pi['personality']
-    t_o=0
-    t_c=0
-    t_e=0
-    t_a=0
-    t_n=0
-    for trait in traits:
-        if(trait['trait_id']=="big5_openness"):
-            t_o = trait['percentile']
-        elif(trait['trait_id']=="big5_conscientiousness"):
-            t_c = trait['percentile']
-        elif(trait['trait_id']=="big5_extraversion"):
-            t_e = trait['percentile']
-        elif(trait['trait_id']=="big5_agreeableness"):
-            t_a = trait['percentile']
-        else:
-            #big5_neuroticism
-            t_n = trait['percentile']
+            print("Reading Response of IBM Watson")
+            print(user_pi)
+            traits = user_pi['personality']
+            t_o=0
+            t_c=0
+            t_e=0
+            t_a=0
+            t_n=0
+            for trait in traits:
+                if(trait['trait_id']=="big5_openness"):
+                    t_o = trait['percentile']
+                elif(trait['trait_id']=="big5_conscientiousness"):
+                    t_c = trait['percentile']
+                elif(trait['trait_id']=="big5_extraversion"):
+                    t_e = trait['percentile']
+                elif(trait['trait_id']=="big5_agreeableness"):
+                    t_a = trait['percentile']
+                else:
+                    #big5_neuroticism
+                    t_n = trait['percentile']
 
-
-    # Append to File Information
-    with open(fileName, "a") as myfile:
-        myfile.write(str(user)+","+str(t_o)+','+str(t_c)+','+str(t_e)+','+str(t_a)+','+str(t_n)+'\n')
+            print("Saving user result ")
+            # Append to File Information
+            with open(fileName, "a") as myfile:
+                myfile.write(str(user)+","+str(len(user_tweets))+","+str(t_o)+','+str(t_c)+','+str(t_e)+','+str(t_a)+','+str(t_n)+'\n')
+        except:
+            # Append to File Information
+            with open(fileName, "a") as myfile:
+                myfile.write(str(user)+","+str(len(user_tweets))+",,,,,"+'\n')
--- services/get_tweets.py
+++ services/get_tweets.py
@@ -9,8 +9,19 @@ class GetTweets:
         client = MongoClient(config.db)
         self.db = client.geo_tweets
 
+    def count_target_users(self):
+        return self.db.target_users.count()
+
+    def get_target_users(self):
+        elements = self.db.target_users.find()
+        #Convert to Tweet Object
+        result = []
+        for doc in elements:
+            result.append(doc['_id'])
+        return result
+
     def get_tweets_from(self, user):
-        elements = self.db.tweets_en.find({'user.id': user})
+        elements = self.db.tweets_en.find({'user.id': user},{"user.id":1,"user.name":1,"user.screen_name":1,"place.name":1,"place.full_name":1,"place.country_code":1,"place.country":1,"lang":1,"created_at":1,"id":1,"text":1})
         #Convert to Tweet Object
         result = []
         for doc in elements:
